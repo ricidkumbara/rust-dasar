361e109130e7895bf0256a0792d214921b8ff529
ricidkumbara <ricidkumbara@gmail.com>
2024-08-10 08:04:02 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -301,3 +301,59 @@ fn if_expression() {
 
     println!("{}", result);
 }
+
+#[test]
+fn loop_expression() {
+    let mut counter = 0;
+
+    loop {
+        counter += 1;
+
+        if counter > 10 {
+            break;
+        } else if counter % 2 == 0 {
+            continue;
+        }
+
+        println!("{}", counter);
+    }
+}
+
+#[test]
+fn loop_return_value() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter > 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("{}", result);
+}
+
+#[test]
+fn loop_label() {
+    let mut number = 1;
+
+    'outer: loop {
+        let mut i = 1;
+
+        loop {
+            if number > 10 {
+                break 'outer;
+            }
+
+            println!("{} x {} = {}", number, i, number * i);
+
+            i += 1;
+            if i > 10 {
+                break;
+            }
+        }
+
+        number += 1;
+    }
+}
